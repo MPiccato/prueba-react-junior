d910061b5da7d29d326cba02db87abc8d2a4106c
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2023-09-30 19:23:43 -0300

    renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
 export function App() {
+
+    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => res.json())
+            .then(data => setFact(data.fact))
+    }, [])
+
     return (
-        <h1>App de gatitos</h1>
+
+        <main>
+            <h1>App de gatitos</h1>
+            {fact && <h3>{fact}</h3>}
+        </main>
+
     )
 }
